a58be01a328d5ddbb5a61ef6822976820a2fe5ad
Floppy-disko <mat4disco@gmail.com>
2022-05-21 10:56:12 +0200

    aggiunti i cmake per chi usa clion o cmake in generale

--- sistemi_operativi/system_call/client_0.c
+++ sistemi_operativi/system_call/client_0.c
@@ -2,8 +2,19 @@
 /// @brief Contiene l'implementazione del client.
 
 #include "defines.h"
+#include<unistd.h>//
+#include<stdio.h>//
 
 int main(int argc, char * argv[]) {
+
+    pid_t p = fork();
+    if(p==-1)
+        errExit("fork failed");
+    if(p==0)
+        printf("\nFiglio");
+    else
+        printf("\nPadre");
+
 
     return 0;
 }
